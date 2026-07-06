985c5120aa54849a975b5b2c5ad63b2cfa4b65d2
Ahmed El-kholy <ahme152005@gmail.com>
2026-07-06 15:21:36 +0300

    Create Middlewares by three ways/overloads by using [Use]

--- M01-Overview/Program.cs
+++ M01-Overview/Program.cs
@@ -1,22 +1,161 @@
 var builder = WebApplication.CreateBuilder(args);
-// DI container 
 
+// ===============================
+// Register Services (DI Container)
+// ===============================
+
+// builder.Services.Add...();
 
 var app = builder.Build();
 
-// middleware setup goes here 
-// framework sets up the pipeline infra when project created 
-// pipeline conceptually empty , through templates may include some default middleware 
-// you can add middleware from framework , 3rd paty library , custom components 
-// the pipeline is a sequence of middleware that process HTTP requests , responses (chain of responsibility by using RequestDelegate)
-// imagine middleware as function take RequestDelegate and return RequestDelegate 
-// RequestDelegate takes HttpContext and return Task 
-// Task -> promise that i will execute and return void  
+// ===============================
+// Configure Middleware Pipeline
+// ===============================
+
+
+/*
+============================================================
+Way #1
+Use(Func<RequestDelegate, RequestDelegate>)
+Factory Style
+------------------------------------------------------------
+Signature:
+
+RequestDelegate -> RequestDelegate
+
+Take:
+    RequestDelegate next
+
+Return:
+    RequestDelegate
+
+Think of it as:
+
+    next  -------->  build new middleware  -------->  return new next
+============================================================
+*/
+
+
+// ----------------------------------------------------------
+// Example 1 : Middleware does absolutely nothing
+// ----------------------------------------------------------
+
+app.Use((RequestDelegate next) =>
+{
+    // Just return the next middleware without modification
+    return next;
+});
+
+// Same as:
+//
+// app.Use(next => next);
+
+
+// ----------------------------------------------------------
+// Example 2 : Create a middleware manually
+// ----------------------------------------------------------
+
+app.Use((RequestDelegate next) =>
+{
+    // Return a new RequestDelegate
+    return async (HttpContext context) =>
+    {
+        await context.Response.WriteAsync("MW #2\n");
+
+        // Pass execution to the next middleware
+        await next(context);
+
+        // Code here executes after the next middleware
+    };
+});
+
+// Same as:
+//
+// app.Use(next =>
+// {
+//     return async context =>
+//     {
+//         await context.Response.WriteAsync("MW #2\n");
+//         await next(context);
+//     };
+// });
+
+
+
+/*
+============================================================
+Way #2
+Use(Func<HttpContext, RequestDelegate, Task>)
+
+Signature:
+
+(HttpContext, RequestDelegate) -> Task
+
+Take:
+    HttpContext context
+    RequestDelegate next
+
+Return:
+    Task
+
+This is the most common overload.
+
+ASP.NET Core builds the RequestDelegate for you.
+============================================================
+*/
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    await context.Response.WriteAsync("MW #1\n");
+
+    // Continue to the next middleware
+    await next(context);
+
+    // This code runs after the next middleware finishes
+});
+
+
+/*
+============================================================
+Way #3
+Use(Func<HttpContext, Func<Task>, Task>)
+
+Signature:
+
+(HttpContext, Func<Task>) -> Task
+
+Take:
+    HttpContext context
+    Func<Task> next
+
+Return:
+    Task
+
+This overload wraps RequestDelegate into Func<Task>.
+
+Instead of:
+
+    await next(context);
+
+you simply write:
+
+    await next();
+
+This overload is less common than Way #2.
+============================================================
+*/
+
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+    await context.Response.WriteAsync("MW #3\n");
+
+    await next();
+
+    // Executes after the remaining pipeline finishes
+});
+
 
 app.Run();
 
 
-// called Middleware of Middleware component
-// middleware must apply SPR (Single Responsibilty Principle) 
-// we will learn three ways to write middleware (3 overloads)
-
+// Note RequestDelegate take HttpContext and return Task
